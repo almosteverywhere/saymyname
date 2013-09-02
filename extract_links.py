38dfa55575b0592e3a297a7d3214dfaf1294fe12
Julie Lavoie <almosteverywhere@gmail.com>
2013-09-02 15:49:33 -0400

    cleanup

--- extract_links.py
+++ extract_links.py
@@ -19,7 +19,6 @@ def generate_song_urls(artist_url):
 
     # ok super ghetto must be faster
     for i in mylist:
-        # url_list.append(base_url + i['href'])
         song_urls.append(base_url + i['href'])
 
     for url in song_urls[:2]:
@@ -38,31 +37,4 @@ def parse_text(song_url):
     lyrics_string = re.sub("\n", " ", lyrics_string)
 
     return lyrics_string
-    # now we can strip out the \n
-    # and split on the space
 
-
-
-    # need a way to get the text and the link text
-    #
-    # # extract the link text
-    # # to run regex
-    # # soup returns a tag object but regex needs a string
-    # for i in lyrics_mess:
-    #     lyrics_string = lyrics_string + " " + str(i)
-    # # here we have all the link text
-    # contents = re.findall('<a .*>(.*)</a>', lyrics_string)
-
-    # we don't care about the order, because all we're doing is counting
-    # the occurences of a word
-
-    # links = lyrics[0].findAll('a')
-    # also gives you all links what all the stuff, and the a href contents as well
-
-    # now we need the stuff outside the links
-    # performance on this thing will be terrible, but let's worry about that later
-
-    # for i in lyrics:
-    # string_lyrics = string_lyrics + " " + i
-
-generate_song_urls(link)
--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python
+from extract_links import *
 import re
-import lxml.html
-mdict = {}
+
 # bad words no one shoudl say, but there you go
 # bad_words = ['bitch', 'nigga' ]
 
 # given some text, match the occurences of bitch, and n-word
 def read_words(stuff, bad_words=['bitch', 'nigga']):
+    mydict = {}
 
     for word in bad_words:
 
         # matches = re.findall('bitch', stuff)
         matches = re.findall(word, stuff)
-        print "you have " + str(len(matches)) + " " + word + " up in here"
+        # print "you have " + str(len(matches)) + " " + word + " up in here"
         # print "you have " + str(len(matches)) + " bitches up in here"
         # mydict['bitches'] = len(matches)
 
         mydict[word] = len(matches)
 
+    return mydict
 
+def print_dict(dict):
+    for key in dict.keys():
+        print str(key) + " : " + str(dict[key]) + "\n"
 
+# stuff = "bitch, whatever, some stuff, bitches, bitch, nigga, niggas, what's up my nigga"
+# read_words(stuff)
 
-# # Open file
-#   f = open('test.txt', 'r')
-#   # Feed the file text into findall(); it returns a list of all the found strings
-#
-# trings = re.findall(r'some pattern', f.read())
-
-stuff = "bitch, whatever, some stuff, bitches, bitch, nigga, niggas, what's up my nigga"
-read_words(stuff)
+text = parse_text("http://rapgenius.com/A-ap-rocky-fuckin-problems-lyrics")
+mydict = read_words(text)
+print_dict(mydict)
