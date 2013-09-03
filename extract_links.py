903f7cf7c3a181c2d6a2c1024dbb1ddecc908cf4
Julie Lavoie <almosteverywhere@gmail.com>
2013-09-03 16:41:45 -0400

    count total, and words

--- extract_links.py
+++ extract_links.py
@@ -3,6 +3,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 import re
+from decimal import Decimal
 
 base_url = "http://rapgenius.com"
 link = "http://rapgenius.com/artists/A-ap-rocky"
@@ -27,7 +28,7 @@ def generate_song_urls(artist_url):
     return song_urls[:2]
 
 # given a song url, return the lyrics as a string
-def parse_text(song_url):
+def get_lyrics(song_url):
     page = urllib2.urlopen(song_url).read()
     soup = BeautifulSoup(page)
     soup.prettify()
@@ -39,7 +40,7 @@ def parse_text(song_url):
     return lyrics_string
 
 # given an artist page with a bunch of links, return urls
-def parse_link_page(url="http://rapgenius.com/artists/A-ap-rocky"):
+def get_song_urls(url="http://rapgenius.com/artists/A-ap-rocky"):
     urls = []
     page = urllib2.urlopen(url).read()
     soup = BeautifulSoup(page)
@@ -60,25 +61,83 @@ def parse_link_page(url="http://rapgenius.com/artists/A-ap-rocky"):
 # bad_words = ['bitch', 'nigga' ]
 
 # given some text, match the occurences of bitch, and n-word
-def read_words(stuff, bad_words=['bitch', 'nigga']):
+def count_words(lyrics, bad_words=['bitch', 'nigga']):
     mydict = {}
 
     for word in bad_words:
 
         # matches = re.findall('bitch', stuff)
-        matches = re.findall(word, stuff)
+        matches = re.findall(word, lyrics)
         # print "you have " + str(len(matches)) + " " + word + " up in here"
         # print "you have " + str(len(matches)) + " bitches up in here"
         # mydict['bitches'] = len(matches)
 
         mydict[word] = len(matches)
 
+    # return a word count as well
+    words = lyrics.split()
+    num_words = len(words)
+    mydict['num_words'] = num_words
+
+    print_dict(mydict)
     return mydict
 
 def print_dict(dict):
     for key in dict.keys():
         print str(key) + " : " + str(dict[key]) + "\n"
 
+# given an artist, give the total count of bad words for all songs
+# for right now it only counts the first page of songs
+def reckoning(artist_url="http://rapgenius.com/artists/A-ap-rocky", bad_words=['bitch', 'nigga']):
 
+# ok this is super ghetto, better to get each iteration to edit the same dict
+    big_dict = {}
+    big_dict['bitch'] = 0
+    big_dict['nigga'] = 0
+    big_dict['num_words'] = 0
 
-parse_link_page()
+# get the list of urls:
+    url_list = get_song_urls("http://rapgenius.com/artists/A-ap-rocky")
+    # for u in url_list:
+    #     print u
+    for u in url_list:
+        print "song: " + u
+        lyrics = get_lyrics(u)
+        small_dict = count_words(lyrics)
+        big_dict['bitch'] += (0 or small_dict['bitch'])
+        big_dict['nigga'] += (0 or small_dict['nigga'])
+        big_dict['num_words'] += (0 or small_dict['num_words'])
+        # print "now we have " + str(big_dict['bitch']) + " bitches"
+        # print "now we have " + str(big_dict['nigga']) + " niggas"
+        small_dict = {}
+
+    print "Total for " + artist_url
+    big_dict_stats(big_dict)
+
+def big_dict_stats(dict):
+    print_dict(dict)
+    p_b = Decimal(dict['bitch']) / Decimal(dict['num_words'])
+    p_n = Decimal(dict['nigga']) / Decimal(dict['num_words'])
+    print "B-word is used " + str(p_b)
+    print "n-word is used " + str(p_n)
+
+# stats we want:
+# per total words
+# so far asap rocky:
+# nigga : 133
+#
+# bitch : 78
+
+# Total for http://rapgenius.com/artists/A-ap-rocky
+# nigga : 133
+#
+# bitch : 78
+#
+# num_words : 11091
+#
+# B-word is used 0.007032729239924262915877738707
+# n-word is used 0.01199170498602470471553511856
+
+# need some kind of memoisation or store the results in a db of some kind
+# more signicant is per number of songs
+# or how many times per song
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import re
 # stuff = "bitch, whatever, some stuff, bitches, bitch, nigga, niggas, what's up my nigga"
 # read_words(stuff)
 
-text = parse_text("http://rapgenius.com/A-ap-rocky-fuckin-problems-lyrics")
-mydict = read_words(text)
-print_dict(mydict)
+reckoning()
+
+
+# text = parse_text("http://rapgenius.com/A-ap-rocky-fuckin-problems-lyrics")
+# mydict = read_words(text)
+# print_dict(mydict)
