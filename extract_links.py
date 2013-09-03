8d4551e20fe4fff08c11bd29aeca852f5aed31c9
Julie Lavoie <almosteverywhere@gmail.com>
2013-09-03 15:47:00 -0400

    Given artist page, return list of song urls

--- extract_links.py
+++ extract_links.py
@@ -38,3 +38,47 @@ def parse_text(song_url):
 
     return lyrics_string
 
+# given an artist page with a bunch of links, return urls
+def parse_link_page(url="http://rapgenius.com/artists/A-ap-rocky"):
+    urls = []
+    page = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(page)
+    soup.prettify()
+    songlist = soup.select("ul.song_list")
+    # gives you all links in the first page of all songs
+    list = songlist[1].select("a")
+    for i in list:
+        urls.append(base_url + i['href'])
+
+    return urls
+    # oh crap, it does a scroll down thing to load more song titles
+    # ok, let's do the first page as a sample, and then we can figure out
+    # what to do for pagination, we can use net console to figure out
+    # what url the ajax is requesting to
+
+# bad words no one shoudl say, but there you go
+# bad_words = ['bitch', 'nigga' ]
+
+# given some text, match the occurences of bitch, and n-word
+def read_words(stuff, bad_words=['bitch', 'nigga']):
+    mydict = {}
+
+    for word in bad_words:
+
+        # matches = re.findall('bitch', stuff)
+        matches = re.findall(word, stuff)
+        # print "you have " + str(len(matches)) + " " + word + " up in here"
+        # print "you have " + str(len(matches)) + " bitches up in here"
+        # mydict['bitches'] = len(matches)
+
+        mydict[word] = len(matches)
+
+    return mydict
+
+def print_dict(dict):
+    for key in dict.keys():
+        print str(key) + " : " + str(dict[key]) + "\n"
+
+
+
+parse_link_page()
--- main.py
+++ main.py
@@ -2,28 +2,6 @@
 from extract_links import *
 import re
 
-# bad words no one shoudl say, but there you go
-# bad_words = ['bitch', 'nigga' ]
-
-# given some text, match the occurences of bitch, and n-word
-def read_words(stuff, bad_words=['bitch', 'nigga']):
-    mydict = {}
-
-    for word in bad_words:
-
-        # matches = re.findall('bitch', stuff)
-        matches = re.findall(word, stuff)
-        # print "you have " + str(len(matches)) + " " + word + " up in here"
-        # print "you have " + str(len(matches)) + " bitches up in here"
-        # mydict['bitches'] = len(matches)
-
-        mydict[word] = len(matches)
-
-    return mydict
-
-def print_dict(dict):
-    for key in dict.keys():
-        print str(key) + " : " + str(dict[key]) + "\n"
 
 # stuff = "bitch, whatever, some stuff, bitches, bitch, nigga, niggas, what's up my nigga"
 # read_words(stuff)
