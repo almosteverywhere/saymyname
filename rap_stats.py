45518dcefa14a58431c9463710516ac02590f959
Julie Lavoie <almosteverywhere@gmail.com>
2013-09-19 21:27:56 -0400

    Ok, now we store all the song info in a big dict. There must be a better data struct.

--- rap_stats.py
+++ rap_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from bs4 import BeautifulSoup
+from collections import Counter
 import re, os 
 
 # where we downloaded the files to 
@@ -41,7 +42,7 @@ def get_lyrics_string(file):
 # given lyrics and a dict of bad words, count the occurrences
 # of each bad word and return a dictionary of results
 # RETURNS THE LYRICS FOR ONE SONGb
-def count_words(lyrics, bad_words=bad_words):
+def make_lyrics_dict(lyrics, bad_words=bad_words):
     mydict = {}
 
     for word in bad_words:
@@ -58,47 +59,64 @@ def print_dict(dict):
     for key in dict.keys():
         print str(key) + " : " + str(dict[key]) + "\n"
 
-# given an artist, give the total count of bad words for all songs
-def count_em_up(artist="Kanye-west", bad_words=bad_words):
-    max_song_key = ""
-    max_num_key = ""
-
-    artist_dict = {}
-    for word in bad_words:
-        # FIXME we should probably be using json
-        # max_song_key = "max_" + word + "_song"
-        # max_num_key =  "max" + word + "_num"
-        artist_dict[word] = 0
-        # artist_dict[max_num_key] = 0
-        # artist_dict[max_song_key] = ""
-
-
-    file_list = get_song_list(artist)
-    num_songs = len(file_list)
-    for f in file_list:
-        print "song: " + f
-        lyrics = get_lyrics_string(f)
-        small_dict = count_words(lyrics)
-
-        for word in bad_words:
-            artist_dict[word] += (0 or small_dict[word])
-
-        # need to reset the small dict
-        small_dict = {}
-
-    # print "Total for " + artist + " over " + str(num_songs) + " songs:"
-    # big_dict_stats(big_dict, num_songs)
-    artist_dict['num_songs'] = num_songs
-    return artist_dict
-
 # do all the artists and put in a big dictionary
-def get_all_artists(dir=basedir):
+def make_big_dict(dir=basedir, testing=False):
     results = {}
     artists = os.listdir(dir)
-    for artist in artists:
-        print "Artist " + artist
-        results[artist] = count_em_up(artist)
+    if testing:
+        for artist in artists[:4]:
+            print "Artist " + artist
+            results[artist] = make_artist_dict(artist)    
+    else: 
+        for artist in artists:
+            print "Artist " + artist
+            results[artist] = make_artist_dict(artist)
     return results
+
+# instead we could have count_em_up return a dict of the songs with the titles
+def make_artist_dict(artist, bad_words=bad_words):
+    # start with empty dictionary
+
+    artist_dict = {}
+    
+    # get the list of all songs for artist, one song per file
+    file_list = get_song_list(artist)
+    # num_songs = len(file_list)
+    for f in file_list:
+        
+        lyrics = get_lyrics_string(f)
+        song_name = os.path.basename(f)
+        # this still returns: 
+        # 'Kanye-west-cant-tell-me-nothing-roc-remix-lyrics.html'
+        # what we want is count words to return a dictionary 
+        artist_dict[song_name] = Counter(make_lyrics_dict(lyrics))
+    return artist_dict
+
+# given a big dict, calculate a bunch of stats and return a dict with the stats?
+# allright this is some magic shit let it be:
+# sorted_page_sequence_list = sorted(page_sequence_dict.items(), key=itemgetter(1), reverse=True)
+def print_stats(dict, results_file="results.txt"):
+    # ok, let's make a dict of results
+    results = {}
+
+    # there must be a less retarded way to do this! 
+    for artist in dict.keys():
+        
+        num_songs = len(dict[artist])
+        results[artist] = {}
+        for word in bad_words: 
+            count = 0 
+            for song in dict[artist].keys():
+                count += dict[artist][song][word]
+            results[artist][word] = "%.2f" % (float(count) / float(num_songs))
+            # average = "%.2f" % average
+        results[artist]["num_songs"] = num_songs
+    return results    
+# for each artist we have to look at each word
+        # results[artist][]
+        # print artist
+        # dict[artist] is a dict of songs with counts for each
+
 
 def calculate_average(dict, bad_word):
     total_words = 0
@@ -108,12 +126,6 @@ def calculate_average(dict, bad_word):
             total_songs += dict[i]['num_songs']
     average = float(total_words)/ float(total_songs)
     return (average, total_words, total_songs)
-
-def slice_dict(dict, keys=[]):
-    # holy fuck this is some magic ish
-    # new_dict = itemgetter(*keys)(results)
-    # oh, doesn't work, returns a tuple.
-    return new_dict
 
 def sliced_average(dict, bad_word, keys=()):
     total_words = 0
