23d953059aa570ae61f3c57adf3b5c898d2a95c2
Julie Lavoie <almosteverywhere@gmail.com>
2013-09-25 19:47:44 -0400

    Added stop words

--- most_frequent_words.py
+++ most_frequent_words.py
@@ -4,9 +4,20 @@ from rap_stats import *
 from collections import Counter
 import re
 
-stop_words = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+# stop_words = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
 
-stop_words = re.split("," stop_words)
+# stop_words = re.split("," stop_words)
+
+stop_file = "stop_words2.txt"
+
+stop_words = []
+
+def get_stop_words(file):
+
+	f = open(file, "r")
+	for word in f.readlines():
+		stop_words.append(word.strip())
+	return stop_words
 
 
 # pass in a list of artists, return a counter of most used words,
